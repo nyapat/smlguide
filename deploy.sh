dbe7ecb8a00e34918363c2f55b7f31c1653cd738
nyapat <73502164+nyapat@users.noreply.github.com>
2024-04-10 20:50:45 +1000

    update deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:nyapat/nyapat.github.io.git master
+git push -f git@github.com:nyapat/smlguide.git master:gh-pages
 
 cd -
